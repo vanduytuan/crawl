a7a249d39bce9af13e2dd17e84876fedcd4ef325
VanDuyTuan <VanDuyTuan@172.20.46.162>
2013-04-13 18:22:47 +0800

    Crawler

--- src/main/Main.java
+++ src/main/Main.java
@@ -4,17 +4,20 @@
  */
 package main;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  *
  * @author VanDuyTuan
  */
 public class Main {
-    
+
     public static void main(String[] args) {
         // TODO code application logic here
-        String url = "";
+        String url = "http://www.yelp.com/biz/brendas-french-soul-food-san-francisco";
+        //url = "http://www.yelp.com/biz/restaurant-ember-singapore";
         Crawler crawler = new Crawler(url);
-        crawler.crawl();
-    }
 
+    }
 }
